53c2ac4049c0d8d02a86c994356437c683c1f2b4
Paweł Martosz <pawel.m476@gmail.com>
2022-10-09 12:16:27 +0200

    Dodanie frontend react

---
+++ Frontend React/aplikacja/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+
+
+class App extends React.Component{
+  state = {
+    elements: [
+   
+    ]
+  }
+
+
+
+  render()
+  {
+    
+      
+      return(
+        <div>
+          <h1>Dziala jest ok</h1>
+        </div>
+      )
+    
+  }
+}
+
+export default App;
