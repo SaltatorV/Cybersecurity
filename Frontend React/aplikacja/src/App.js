971705537197ae0b76df71945e6f4fd1457602c4
Paweł Martosz <pawel.m476@gmail.com>
2022-10-09 20:18:30 +0200

    test2

--- Frontend React/aplikacja/src/App.js
+++ Frontend React/aplikacja/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 import './App.css';
 
 
@@ -19,6 +20,27 @@ class App extends React.Component{
       return(
         <div>
           <h1>Dziala jest ok</h1>
+
+          <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
         </div>
       )
     
