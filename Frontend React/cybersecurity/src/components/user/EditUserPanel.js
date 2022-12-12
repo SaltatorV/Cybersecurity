4beef13cef221dc9718bf22b7db5fa8662fc9bf9
Paweł Martosz <pawel.m476@gmail.com>
2022-12-12 19:35:45 +0100

    Update EditUserPanel.js

--- Frontend React/cybersecurity/src/components/user/EditUserPanel.js
+++ Frontend React/cybersecurity/src/components/user/EditUserPanel.js
@@ -7,9 +7,9 @@ function EditUserPanel(props) {
   const [user, setUser] = useState();
   const [formData, setfromData] = useState([]);
   const [show, setShow] = useState(0)
+  const [select,setSelect] = useState("");
 
-
-  console.log(props.id)
+  
   const columns = [
     {
       Index: 1,
@@ -18,43 +18,28 @@ function EditUserPanel(props) {
     },
     {
       Index: 2,
-      Header: 'Rola',
-      Accessor: 'roleName',
-    },
-    {
-      Index: 3,
       Header: 'IsPasswordExpire',
       Accessor: 'isPasswordExpire',
     },
     {
-      Index: 4,
-      Header: 'PasswordExpire',
-      Accessor: 'passwordExpire',
+      Index: 3,
+      Header: 'DayExpire',
+      Accessor: 'dayExpire',
     },
     {
       Index: 5,
-      Header: 'MaxFailLogin',
-      Accessor: 'maxFailLogin',
+      Header: 'RoleId',
+      Accessor: 'roleId',
     },
     {
       Index: 6,
-      Header: 'IsLoginLockOn',
-      Accessor: 'isLoginLockOn',
-    },
-    {
-      Index: 7,
-      Header: 'LoginLockOnTime',
-      Accessor: 'loginLockOnTime',
-    },
-    {
-      Index: 8,
       Header: 'SessionTime',
       Accessor: 'sessionTime',
     },
     {
-      Index: 9,
-      Header: 'IsOneTimePasswordSet',
-      Accessor: 'isOneTimePasswordSet',
+      Index: 7,
+      Header: 'MaxFailLogin',
+      Accessor: 'maxFailLogin',
     },
   ];
 
@@ -81,31 +66,99 @@ function EditUserPanel(props) {
   }, []);
 
   const handleChange = (e) => {
-    setfromData({
-      ...formData,
-      [e.target.name]: e.target.value,
-
-    })
-    console.log(e.target.value);
-
+   
+ 
+    
+    if(e.target.value === "true")
+    {
+      setSelect(true);
+    }
+    else if (e.target.value === "false")
+    {
+      setSelect(false);
+    }
+    else
+    {
+      setSelect(e.target.value);
+    }
   }
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     //funkcja do edycji itp........
 
+    console.log(e.target.name);
 
-    setShow(0);
-  }
+    var userPostEdit = {
+      Id:user.id,
+      Login:user.login,
+      IsPasswordExpire:user.isPasswordExpire,
+      DayExpire:user.DayExpire,
+      RoleId:user.RoleId,
+      SessionTime:user.SessionTime,
+      MaxFailLogin:user.MaxFailLogin
+    }
+    
+    
+    for (const [key, value] of Object.entries(userPostEdit)) {
+        //console.log(`${key}: ${value}`);
+      //console.log(key);
+      if(key === e.target.name)
+      {
+        console.log("key",key);
+        console.log("value",value);
+        console.log("set Value",select)
+        console.log(userPostEdit[key]);
+        
+        userPostEdit[key] = select;
+             
+    }}
+    console.log(userPostEdit)
+    
+    await fetch("http://localhost:7277/api/account/update/"+user.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(userPostEdit),
+    })
+      .then((response) => {
+        console.log(response.status);
+        return response.json();
+      })
+      .then((res) => {
+        window.location.href = "/";
+      })
+      .catch((error) => console.log(error));
+      setShow(0);
+  };
+
+  
+    
+  
   const edit = (name) => {
     return (<div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} name={name}>
         <label>
           {name}:
           <input type="text" login="login" onChange={handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form></div>)
+  }
+  const editSelect = (name) => {
+    return (<div>
+    <form onSubmit={handleSubmit} name={name}>
+    <select onChange={handleChange} >
+    <option value="">--Please choose an option--</option>
+    <option value={true} >true</option>
+    <option value={false}>false</option>
+</select>
+<input type="submit" value="Submit" />
+</form>
+    </div>)
   }
 
   return (
@@ -130,40 +183,27 @@ function EditUserPanel(props) {
                     {(show === 1) && (edit("Login"))}
                   </td>
 
-                  <td>{user.roleName}
+                  <td>{user.isPasswordExpire.toString()}
                     {(show === 0) && (<button onClick={() => setShow(2)}>Edit</button>)}
-                    {(show === 2) && (edit("roleName"))}
+                    {(show === 2) && (editSelect("IsPasswordExpire"))}
 
                   </td>
-                  <td>{user.isPasswordExpire ? 'tak' : 'nie'}
+                  <td>{user.dayExpire}
                     {(show === 0) && (<button onClick={() => setShow(3)}>Edit</button>)}
-                    {(show === 3) && (edit("isPasswordExpire"))}
+                    {(show === 3) && (edit("DayExpire"))}
                   </td>
-                  <td>{user.passwordExpire}
+                  <td>{user.roleId}
                     {(show === 0) && (<button onClick={() => setShow(4)}>Edit</button>)}
-                    {(show === 4) && (edit("passwordExpire"))}
+                    {(show === 4) && (edit("RoleId"))}
+                  </td>
+                  <td>{user.SessionTime}
+                    {(show === 0) && (<button onClick={() => setShow(5)}>Edit</button>)}
+                    {(show === 5) && (edit("SessionTime"))}
                   </td>
                   <td>{user.maxFailLogin}
-                    {(show === 0) && (<button onClick={() => setShow(5)}>Edit</button>)}
-                    {(show === 5) && (edit("maxFailLogin"))}
-                  </td>
-                  <td>{user.isLoginLockOn ? 'tak' : 'nie'}
                     {(show === 0) && (<button onClick={() => setShow(6)}>Edit</button>)}
-                    {(show === 6) && (edit("isLoginLockOn"))}
+                    {(show === 6) && (edit("MaxFailLogin"))}
                   </td>
-                  <td>{user.loginLockOnTime}
-                    {(show === 0) && (<button onClick={() => setShow(7)}>Edit</button>)}
-                    {(show === 7) && (edit("loginLockOnTime"))}
-                  </td>
-                  <td>{user.sessionTime}
-                    {(show === 0) && (<button onClick={() => setShow(8)}>Edit</button>)}
-                    {(show === 8) && (edit("sessionTime"))}
-                  </td>
-                  <td>{user.isOneTimePasswordSet ? 'tak' : 'nie'}
-                    {(show === 0) && (<button onClick={() => setShow(9)}>Edit</button>)}
-                    {(show === 9) && (edit("isOneTimePasswordSet"))}
-                  </td>
-                  
                 </tr>
               </tbody>
             </table>
